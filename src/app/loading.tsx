f61a781684159b3cdb3f7f6577ef4c86e45f0c15
Junaid Razzaq <142839488+junaidrazzaq84@users.noreply.github.com>
2024-10-23 01:27:47 +0500

    Delete src/app directory

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoadingPage() {
-  return (
-    <div>
-      <h1 className="bg-red-800 font-bold text-yellow-400">
-        Please Wait This Page is Loading....
-      </h1>
-    </div>
-  );
-}
--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div>
-      <h1 className="bg-gray-400 text-justify font-bold border-8 border-blue-800 text-emerald-950">
-        Page Not Found Please check other Pages
-      </h1>
-    </div>
-  );
-}
