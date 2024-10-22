6431cb30ebe63f028d8f045b8b3623e1670acc11
Junaid Razzaq <142839488+junaidrazzaq84@users.noreply.github.com>
2024-10-23 01:30:40 +0500

    Add files via upload

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="bg-slate-800 text-yellow-400 font-bold border-8 rounded-full border-yellow-500 p-2 m-3">
+        Contact Us Page
+      </h1>
+      <p className="bg-gray-400 text-justify font-bold border-8 border-blue-800 text-emerald-950">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas,
+        vitae recusandae quia ut rerum dolore dicta nihil saepe quam eos ratione
+        ab distinctio voluptatum vero illum ullam excepturi placeat iste. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eius
+        debitis quibusdam illum perspiciatis non modi, placeat quasi soluta
+        ducimus quam asperiores magnam porro quae tempora sed animi, sint hic.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function LoadingPage() {
+  return (
+    <div>
+      <h1 className="bg-red-800 font-bold text-yellow-400">
+        Please Wait This Page is Loading....
+      </h1>
+    </div>
+  );
+}
